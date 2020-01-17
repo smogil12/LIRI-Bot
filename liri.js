492d96f664cef74bc85520d1ac21d752ef7148bc
Spencer Mogil <smogil12@gmail.com>
2020-01-17 17:38:05 -0500

    feat: add movieThis, spotifyCall, concertThis constructors and fs.readFile

--- liri.js
+++ liri.js
@@ -11,3 +11,24 @@ var query = process.argv[3];
 var option = process.argv[2];
 
 var spotify = new Spotify(keys.spotify);
+
+switch (option) {
+  case "movie-this":
+    movieThis(query);
+    break;
+  case "spotify-this-song":
+    spotifyCall(query);
+    break;
+  case "concert-this":
+    concertThis(query);
+    break;
+  default:
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      var data = data.split(",");
+      var thatWay = data[1];
+      if (error) {
+        return console.log(error);
+      }
+      spotifyCall(thatWay);
+    });
+}

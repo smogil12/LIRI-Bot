260d14ce0f2ca9e9b32d9f735364125464a84d8b
Spencer Mogil <smogil12@gmail.com>
2020-01-17 17:22:28 -0500

    chore: add require variables

--- liri.js
+++ liri.js
@@ -2,4 +2,9 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
+var Spotify = require("node-spotify-api");
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
+
 var spotify = new Spotify(keys.spotify);

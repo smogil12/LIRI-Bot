582158ccac3c370cfebc3b2457b00f5b21853403
Spencer Mogil <smogil12@gmail.com>
2020-01-17 17:23:51 -0500

    chore: add process js

--- liri.js
+++ liri.js
@@ -7,4 +7,7 @@ var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
 
+var query = process.argv[3];
+var option = process.argv[2];
+
 var spotify = new Spotify(keys.spotify);
